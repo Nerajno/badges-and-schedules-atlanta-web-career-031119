b7c84997c3ad1014431593a75a01760c926a23d5
active-object-6703 <nerajno@gmail.com>
2019-01-31 04:35:40 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+
+#badge_maker
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+#batch_badge_creator
+def batch_badge_creator
+
+end
+
+#assign_rooms
+def assign_rooms
+
+end
+
+#printer
+def printer
+
+end
