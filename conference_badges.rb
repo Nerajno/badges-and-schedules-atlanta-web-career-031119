dbd06b0cce6e4f98a01b949e201bb422da0efac2
active-object-6703 <nerajno@gmail.com>
2019-02-01 12:40:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -30,4 +30,8 @@ def printer(attendees)
     batch_badge_creator(attendees).each do |badge|
       puts badge
     end
+
+    assign_rooms(attendees).each do |assignment|
+  puts assignment
+end
 end
